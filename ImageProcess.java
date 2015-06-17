193a109c4a9fabb37d4a6bb8b2099dc1dd4ca453
LiShihao <397044795@qq.com>
2015-06-17 19:59:41 +0800

    image process

--- ImageProcess.java
+++ ImageProcess.java
@@ -12,7 +12,69 @@ public class ImageProcess {
 	 */
 	public static void colorAdjustment(String type, int[] pixels, int width,
 			int height) {
-		
+		int i = 0;
+		int total = width * height;
+		if (type.equals("happy")) {
+			for (; i < total; i++) {
+				int r = (int) (pixels[i] & 0xff0000) >> 16;
+				int g = (int) (pixels[i] & 0x00ff00) >> 8;
+				int b = (int) (pixels[i] & 0xff);
+
+				r = (int) (r * 1.6);
+				if (r > 255)
+					r = 255;
+				g = (int) (g * 1.4);
+				if (g > 255)
+					g = 255;
+				b = (int) (b * 1.4);
+				if (b > 255)
+					b = 255;
+
+				pixels[i] = (0xff << 24) | (r << 16) | (g << 8) | b;
+			}
+		} else if (type.equals("sad")) {
+			for (; i < total; i++) {
+				int r = (int) (pixels[i] & 0xff0000) >> 16;
+				int g = (int) (pixels[i] & 0x00ff00) >> 8;
+				int b = (int) (pixels[i] & 0xff);
+
+				b = (int) (b * 1.4);
+				if (b > 255)
+					b = 255;
+
+				pixels[i] = (0xff << 24) | (r << 16) | (g << 8) | b;
+			}
+		} else if (type.equals("memory")) {
+			for (; i < total; i++) {
+				int r = (int) (pixels[i] & 0xff0000) >> 16;
+				int g = (int) (pixels[i] & 0x00ff00) >> 8;
+				int b = (int) (pixels[i] & 0xff);
+
+				r = (int) (r * 1.4);
+				if (r > 255)
+					r = 255;
+				g = (int) (g * 1.3);
+				if (g > 255)
+					g = 255;
+
+				pixels[i] = (0xff << 24) | (r << 16) | (g << 8) | b;
+			}
+		} else if (type.equals("victory")) {
+			for (; i < total; i++) {
+				int r = (int) (pixels[i] & 0xff0000) >> 16;
+				int g = (int) (pixels[i] & 0x00ff00) >> 8;
+				int b = (int) (pixels[i] & 0xff);
+
+				r = (int) (r * 1.8);
+				if (r > 255)
+					r = 255;
+				g = (int) (g * 1.1);
+				if (g > 255)
+					g = 255;
+
+				pixels[i] = (0xff << 24) | (r << 16) | (g << 8) | b;
+			}
+		}
 	}
 
 	public static Meterial renderImage(Meterial renderImage,
