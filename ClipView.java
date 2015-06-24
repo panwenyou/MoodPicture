25a4b229afc457c3d30006795c473c3d1c8b2cff
LiShihao <397044795@qq.com>
2015-06-24 21:50:02 +0800

    java files

--- ClipView.java
+++ ClipView.java
@@ -104,3 +104,109 @@ public class ClipView extends View {
 		canvas.drawRect(left_margin, top_margin, right_margin, bottom_margin, bar_paint);
 	}
 }
+package com.panwenyou.moodpicture;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ClipView extends View {
+
+	public int top_margin = 0;
+	public int left_margin = 0;
+	public int right_margin = 0;
+	public int bottom_margin = 0;
+	
+	public int top_bound = 0;
+	public int left_bound = 0;
+	public int right_bound = 0;
+	public int bottom_bound = 0;
+	
+	private Paint shadow_paint = new Paint();
+	private Paint bar_paint = new Paint();
+	
+	public ClipView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClipView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClipView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void initProperties(int top, int left, int right, int bottom) {
+		top_bound = top;
+		left_bound = left;
+		right_bound = right;
+		bottom_bound = bottom;
+		
+		updateProperties(top_bound, left_bound, right_bound, bottom_bound);
+		invalidate();
+	}
+	
+	public void updateProperties(int top, int left, int right, int bottom) {
+		if (top < top_bound) {
+			top_margin = top_bound;
+		} else if (top > bottom) {
+			// do nothing
+		} else {
+			top_margin = top;
+		}
+		
+		if (left < left_bound) {
+			left_margin = left_bound;
+		} else if (left > right) {
+			// do nothing
+		} else {
+			left_margin = left;
+		}
+		
+		if (bottom > bottom_bound) {
+			bottom_margin = bottom_bound;
+		} else if (top > bottom) {
+			// do nothing
+		} else {
+			bottom_margin = bottom;
+		}
+		
+		if (right > right_bound) {
+			right_margin = right_bound;
+		} else if (left > right) {
+			// do nothing
+		} else {
+			right_margin = right;
+		}
+		
+		invalidate();
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+		
+		// Draw shadows
+		shadow_paint.setAlpha(200);
+		canvas.drawRect(0, 0, this.getWidth(), top_margin, shadow_paint);
+		canvas.drawRect(0, top_margin, left_margin, this.getHeight(), shadow_paint);
+		canvas.drawRect(right_margin, top_margin, this.getWidth(), this.getHeight(), shadow_paint);
+		canvas.drawRect(left_margin, bottom_margin, right_margin, this.getHeight(), shadow_paint);
+		
+		// Draw Bars
+		bar_paint.setStyle(Style.STROKE);
+		bar_paint.setColor(Color.WHITE);
+		bar_paint.setStrokeWidth(2);
+		
+		canvas.drawRect(left_margin, top_margin, right_margin, bottom_margin, bar_paint);
+	}
+}
--- ShareActivity.java
+++ ShareActivity.java
@@ -58,3 +58,63 @@ public class ShareActivity extends Activity {
 		});
 	}
 }
+package com.panwenyou.moodpicture;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ShareActivity extends Activity {
+	
+	private String title;
+	private String content;
+	private Uri uri;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		setContentView(R.layout.share_layout);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_layout);
+		
+		((TextView)findViewById(R.id.title_step)).setText("你要说点什么？");
+		((TextView)findViewById(R.id.title_next)).setText("分享");
+		Bitmap bitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/tempimg.jpg");
+		((ImageView)findViewById(R.id.share_img)).setImageBitmap(bitmap);
+		
+		findViewById(R.id.title_next).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				//uri = getIntent().getParcelableExtra("Uri");
+				uri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "tempimg.jpg"));
+				title = ((EditText)findViewById(R.id.share_title)).getText().toString();
+				content = ((EditText)findViewById(R.id.share_content)).getText().toString();
+				
+				Intent share_intent = new Intent(Intent.ACTION_SEND);
+				share_intent.setType("image/jpg");
+				share_intent.putExtra(Intent.EXTRA_STREAM, uri);
+				share_intent.putExtra(Intent.EXTRA_SUBJECT, "分享");
+				share_intent.putExtra(Intent.EXTRA_TITLE, title);
+				share_intent.putExtra(Intent.EXTRA_TEXT, content);
+				share_intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(Intent.createChooser(share_intent, "请选择"));
+			}
+		});
+	}
+}
